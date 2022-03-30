9182ccdf408acf564a4061dcacd11f6d53ab4d78
Brid Callaghan <bridcallaghan56@gmail.com>
2022-03-30 22:19:04 +0100

    pandas

--- analysis.py
+++ analysis.py
@@ -6,9 +6,21 @@
 
 import csv
 
-filename = "iris.data"
+#filename = "iris.csv"
 
-with open(filename, "rt") as csvFile:
-    csvReader = csv.reader(csvFile, delimiter=",")
-    for line in csvReader:
-        print (line[2]) #that works
+#with open(filename, "rt") as csvFile:
+   # csvReader = csv.reader(csvFile, delimiter=",")
+   # for line in csvReader:
+   #  print (line[2]) #that works
+
+#because I am working with tabular data in Python I will use (import) the Python Data Analysis Library  
+#ref https://datacarpentry.org/python-ecology-lesson/02-starting-with-data/
+import pandas as pd
+#creating a dataframe of the iris csv
+iris_df=pd.read_csv("iris.csv")
+#testing how the df looks
+#print (iris_df)
+
+print (iris_df.columns)
+
+
